a7fe3c0fc550457933c8b9b2d76828692923bc2c
Jishnu Mohan <jishnu7@gmail.com>
2020-05-31 13:04:00 +0530

    vuepress config

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: 'Supported Languages', link: '/#languages' },
+      { text: 'Source Code', link: '/#source' },
+      { text: 'What\' New', link: '/whats-new' },
+      { text: 'FAQ', link: '/faq' },
+      { text: 'Contact Us', link: '/#contact' },
+    ]
+  }
+}
