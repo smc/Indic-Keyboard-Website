1442e779312da5eeb1d07811759978a1de3b5e19
Jishnu Mohan <jishnu7@gmail.com>
2020-08-01 20:40:23 +0530

    add icons

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,15 @@
 module.exports = {
   title: 'Indic Keyboard',
   dest: 'public',
+  description: "Indic keyboard brings 23 Indian languages to your mobile along with spell checking, word prediction and transliteration.",
+  head: [
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/logo_32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/logo_16.png"}],
+    ['link', { rel: "icon", type: "image/svg+xml", sizes: "any", href: "/logo.svg"}],
+    ['link', { rel: "apple-touch-icon", sizes: "192x192", href: "/logo_192.png"}],
+    ['meta', { name: "author", content: "Jishnu Mohan"}],
+    ['meta', { name: "theme-color", content: "#4CAF50"}],
+  ],
   themeConfig: {
     logo: '/logo.png',
     repo: 'https://gitlab.com/indicproject/indic-keyboard',
